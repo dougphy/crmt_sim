08d0db31529fab1adc8095e4a96929085b2eec37
Douglas Davis <douglasdavis@utexas.edu>
2013-10-16 00:34:44 -0500

    added some algorithms to evg

--- src/evg.cxx
+++ src/evg.cxx
@@ -1,6 +1,9 @@
 #include <map>
 #include <vector>
+#include <cmath>
 #include "evg.h"
+
+#define PI 3.14159
 
 evg::evg() {}  // Default constructor -- not used
 
@@ -18,6 +21,126 @@ evg::evg(std::string file_name, int n_events)
 
 evg::~evg() {} // Default destructor
 
+void evg::Multiplex(int fiberid, std::vector<int> *ids)
+{
+  int r_num, c_num;
+  r_num = (int)((double)fiberid/64.0);
+  c_num = fiberid - r_num*64;
+
+  int b_num  = (int)((double)r_num/4.0);
+  int r_type = r_num - b_num*4;
+
+  int octuple_index = (int)((double)c_num/8.0);
+  int first_col = octuple_index*8;
+
+  int local_fiber_id;
+
+  switch(r_type) {
+  case 0: {
+    for ( int icol = 0; icol < 8; icol++ ) {
+      local_fiber_id = r_num*64 + first_col + icol;
+      ids->push_back(local_fiber_id);
+    }
+    break;
+  }
+  case 1: {
+    int delta_col = c_num - first_col;
+    for (int icol = 0; icol < 8; icol++) {
+      local_fiber_id = r_num + delta_col + icol*8;
+      ids->push_back(local_fiber_id);
+    }
+    break;
+  }
+  case 2: {
+    int delta_col = c_num - first_col;
+    for (int icol = 0; icol < 8; icol++) {
+      local_fiber_id = r_num + delta_col + icol*8;
+      ids->push_back(local_fiber_id);
+    }
+    break;
+  }
+  case 3: {
+    int delta_col = c_num - first_col;
+    for (int icol = 0; icol < 8; icol++) {
+      local_fiber_id = r_num + delta_col + icol*8;
+      ids->push_back(local_fiber_id);
+    }
+    break;
+  }
+  }
+  return;
+}
+
+bool evg::Intersection(double fx, double fy, double fz,
+		       bool yzView, double tx, double ty, double tz,
+		       double thetaxz, double thetayz,
+		       double sigma_x, double sigma_y, double sigma_z)
+{
+  double TrajX = sin(thetayz*PI/180.);
+  double TrajY = sin(thetaxz*PI/180.);
+  double TrajZ = sqrt(1-pow(TrajX,2)-pow(TrajY,2));
+
+  double top_edge, bottom_edge;
+  top_edge    = fz + sigma_z/2;
+  bottom_edge = fz - sigma_z/2;
+
+  if(yzView) {
+    double left_edge  = fy - sigma_y/2.;
+    double right_edge = fy + sigma_y/2.;
+    double slope      = TrajZ/TrajY;
+    double left_z     = tz + (left_edge-ty)*slope;
+    double right_z    = tz + (right_edge-ty)*slope;
+    double top_z, bottom_z;
+    if (left_z > right_z) {
+      top_z = left_z;
+      bottom_z = right_z;
+    }
+    else {
+      top_z = right_z;
+      bottom_z = left_z;
+    }
+    if (top_edge < bottom_z)
+      return false;
+    if (bottom_edge > top_z)
+      return false;
+    return true;
+  }
+  double left_edge   = fx - sigma_x/2.;
+  double right_edge  = fx + sigma_x/2.;
+  double slope       = TrajZ/TrajX;
+  double left_z      = tz + (left_edge-tx)*slope;
+  double right_z     = tz + (right_edge-tx)*slope;
+  double top_z, bottom_z;
+  if (left_z > right_z) {
+    top_z = left_z;
+    bottom_z = right_z;
+  }
+  else {
+    top_z = right_z;
+    bottom_z = left_z;
+  }
+  if (top_edge < bottom_z)
+    return false;
+  if (bottom_edge > top_z)
+    return false;
+  return true;
+}
+
+int evg::GetFiberInView(int irow, int ifiber, bool YZview)
+{
+  int imodule = (int)((double)irow/4.);
+  int ifiber_in_view = ifiber;
+  if (YZview) {
+    if ( imodule == 2 )
+      ifiber_in_view -= 64*4;
+  }
+  else {
+    ifiber_in_view -= 64*4;
+    if ( imodule == 3)
+      ifiber_in_view -= 64*4;
+  }
+  return ifiber_in_view;
+}
 
 void evg::SaveFile()
 {
--- src/evg.h
+++ src/evg.h
@@ -4,25 +4,31 @@
 #include "TFile.h"
 #include "TTree.h"
 #include <vector>
+#include <string>
 
 class evg {
   
 private:
-  TFile    *fFile;
-  TTree    *fTree;
-  int       fNEvents;
-  int       fEventID;
-  double    fThetaXZ;
-  double    fThetaYZ;
-  int       fTrueFibers[1024];
-  int       fSimFibers[1024];
+  TFile            *fFile;
+  TTree            *fTree;
+  int               fNEvents;
+  int               fEventID;
+  double            fThetaXZ;
+  double            fThetaYZ;
+  int               fTrueFibers[1024];
+  int               fSimFibers[1024];
+  std::vector<int>  fIDs;
   
 public:
   evg();
   evg(std::string fname, int n_events);
   ~evg();
-
-
+  void Multiplex(int fiberid, std::vector<int> *ids);
+  bool Intersection(double fx, double fy, double fz,
+		    bool yzView, double tx, double ty, double tz,
+		    double thetaxz, double thetayz,
+		    double sigma_x, double sigma_y, double sigma_z);  
+  int GetFiberInView(int irow, int ifiber, bool YZview);
   void SaveFile();
 };
 
