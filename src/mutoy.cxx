09609f5d315ba28838b4916de3f35b4ba4381480
Doug Davis <douglasdavis@utexas.edu>
2013-10-26 17:28:30 -0500

    messing with angles

--- src/mutoy.cxx
+++ src/mutoy.cxx
@@ -18,13 +18,38 @@ int main(int argc, char *argv[])
   event_set->ReadParameters();
   event_set->CheckParameters();
   event_set->RunEvents();
-  return 0;
 
   std::cout << "\n\n";
 
+
+  std::cout << "XZ to the RIGHT\n"
+	    << "YZ to the RIGHT\n";
   Line *l = new Line();
   l->SetInitialPos(30.0,30.0,700.0);
-  l->SetLinePropertiesFromPhiTheta(PI/2.,PI/12.);
+  l->SetLinePropertiesFromPhiTheta(PI/4.,PI/12.);
+  double xz = l->AngleXZ();
+  double yz = l->AngleYZ();
+  std::cout << "xz: " << xz << std::endl;
+  std::cout << "yz: " << yz << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "XZ to the LEFT\n"
+	    << "YZ to the RIGHT\n";
+  Line *ll = new Line();
+  ll->SetInitialPos(1,1,1);
+  ll->SetLinePropertiesFromPhiTheta(3*PI/4.,PI/12);
+  yz = ll->AngleYZ();
+  xz = ll->AngleXZ();
+  std::cout << "xz: " << xz << std::endl;
+  std::cout << "yz: " << yz << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  
+
+  return 0;
+
 
 }
 
