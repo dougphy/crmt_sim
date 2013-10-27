7d957841c201c693583263b19a58555e2f4d4119
Doug Davis <douglasdavis@utexas.edu>
2013-10-27 15:22:54 -0500

    doign testing something weird ask fede

--- src/mutoy.cxx
+++ src/mutoy.cxx
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
   std::cout << "\n\n";
 
 
-  std::cout << "XZ to the RIGHT\n"
-	    << "YZ to the RIGHT\n";
+  std::cout << "XZ to the RIGHT (+)\n"
+	    << "YZ to the RIGHT (+)\n";
   Line *l = new Line();
   l->SetInitialPos(30.0,30.0,700.0);
   l->SetLinePropertiesFromPhiTheta(Pi/4.,Pi/12.);
@@ -34,11 +34,11 @@ int main(int argc, char *argv[])
   std::cout << std::endl;
   std::cout << std::endl;
 
-  std::cout << "XZ to the LEFT\n"
-	    << "YZ to the RIGHT\n";
+  std::cout << "XZ to the LEFT (-)\n"
+	    << "YZ to the RIGHT (+)\n";
   Line *ll = new Line();
   ll->SetInitialPos(1,1,1);
-  ll->SetLinePropertiesFromPhiTheta(3*Pi/4.,Pi/12);
+  ll->SetLinePropertiesFromPhiTheta(3*Pi/4.,Pi/12.);
   yz = ll->AngleYZ();
   xz = ll->AngleXZ();
   std::cout << "xz: " << xz << std::endl;
@@ -46,7 +46,31 @@ int main(int argc, char *argv[])
   std::cout << std::endl;
   std::cout << std::endl;
 
-  
+ 
+  std::cout << "XZ to the LEFT (-)\n"
+	    << "YZ to the LEFT (-)\n";
+  Line *lll = new Line();
+  lll->SetInitialPos(0,0,0);
+  lll->SetLinePropertiesFromPhiTheta(5*Pi/4.,Pi/12.);
+  yz = lll->AngleYZ();
+  xz = lll->AngleXZ();
+  std::cout << "xz: " << xz << std::endl;
+  std::cout << "yz: " << yz << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "XZ to the RIGHT (+)\n"
+	    << "YZ to the LEFT (-)\n";
+  Line *llll = new Line();
+  llll->SetInitialPos(0,0,0);
+  llll->SetLinePropertiesFromPhiTheta(7*Pi/4.,Pi/12.);
+  yz = llll->AngleYZ();
+  xz = llll->AngleXZ();
+  std::cout << "xz: " << xz << std::endl;
+  std::cout << "yz: " << yz << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+ 
 
   return 0;
 
