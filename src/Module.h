e14eb496dea8b8947deb3a323225611694c11546
Douglas Davis <douglasdavis@utexas.edu>
2013-11-01 18:23:51 -0500

    removed all consts

--- src/Module.h
+++ src/Module.h
@@ -16,12 +16,12 @@ class Module {
   
 private:
   int          fModuleType;
-  const double fScintWidth  = 10.40;
-  const double fScintHeight = 30.80;
-  const double fStripShift  = 0.333;
-  const double fScintGap    = 4.800;
-  const double fModGap      = 6.400;
-  const double fScintShift  = 0.333;
+  double       fScintWidth  = 10.40;
+  double       fScintHeight = 30.80;
+  double       fStripShift  = 0.333;
+  double       fScintGap    = 4.800;
+  double       fModGap      = 6.400;
+  double       fScintShift  = 0.333;
   double       fGap;
   
   std::map<int, std::pair<double,double> > fFiberMap;
