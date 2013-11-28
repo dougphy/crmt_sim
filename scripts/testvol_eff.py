44bee1653d9f97cc0c1da557e372c85be971a1e0
Douglas Davis <douglasdavis@utexas.edu>
2013-11-28 12:21:54 -0500

    testvol_eff update

--- scripts/testvol_eff.py
+++ scripts/testvol_eff.py
@@ -5,6 +5,15 @@ import sys
 
 the_file = ROOT.TFile(sys.argv[1],'read')
 the_tree = the_file.Get('SimulationTree')
+vol_tree = the_file.Get('TestVolumeTree')
+
+for info in vol_tree:
+    tv_type   = info.TVType
+    tv_center = info.TVCenter
+    tv_radius = info.TVRadius
+    tv_length = info.TVLength
+    tv_width  = info.TVWidth
+    tv_height = info.TVHeight
 
 total = 0
 frac  = 0
@@ -15,9 +24,12 @@ for event in the_tree:
             frac = frac + 1
         total = total + 1
 
-print '---------------------'
+print 'Type:',tv_type
+if tv_type == 'sphere':
+    print 'Radius = ',tv_radius
+if tv_type == 'box':
+    print 'Length, width, height = ',tv_length,tv_width,tv_height
 print 'Det coincidence / TV coincidence'
 print frac,'/',total
 efficiency = float(frac)/float(total)
 print 'Efficiency =',efficiency
-print '---------------------'
